8b584057f98d1a4873aed226adc4235846df2923
Krzysztof Borucki <kbbk23@wp.pl>
2024-03-03 20:14:40 +0100

    some changes in bindEvents

--- JS/script.js
+++ JS/script.js
@@ -20,15 +20,14 @@ const toggleTaskDone = (taskIndex) => {
 };
 
 const bindEvents = () => {
-    const taskList = document.querySelector(".taskList");
+    const taskList = document.querySelector(".js-staskList");
     taskList.addEventListener("click", (event) => {
-        const target = event.target;
-        const parentLi = target.parentElement;
-
-        if (target.classList.contains("js-delete")) {
-            removeTask([...taskList.children].indexOf(parentLi));
-        } else if (target.classList.contains("js-done")) {
-            toggleTaskDone([...taskList.children].indexOf(parentLi));
+        if (event.target.classList.contains("js-delete")) {
+            const taskIndex = [...taskList.children].indexOf(event.target.parentElement);
+            removeTask(taskIndex);
+        } else if (event.target.classList.contains("js-done")) {
+            const taskIndex = [...taskList.children].indexOf(event.target.parentElement);
+            toggleTaskDone(taskIndex);
         }
     });
 };
